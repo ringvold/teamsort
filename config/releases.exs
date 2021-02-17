8d427add71f036942c3e0883120a906efea061ba
Harald Ringvold <harald.ringvold@gmail.com>
2021-02-17 16:14:26 +0100

    Remove cloudbuild config and set url through env

--- config/releases.exs
+++ config/releases.exs
@@ -4,4 +4,4 @@ config :teamsort, TeamsortWeb.Endpoint,
   server: true,
   # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("HOST_NAME"), port: 443]
+  url: [host: System.get_env("DOMAIN"), port: 443]
