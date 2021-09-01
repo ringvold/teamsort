3dbcbdea004494a28663de12b37bc1ef7fd2ddc2
Harald Ringvold <harald.ringvold@gmail.com>
2021-09-02 01:18:01 +0200

    Set up for fly.io

--- config/dev.exs
+++ config/dev.exs
@@ -12,13 +12,15 @@ config :teamsort, TeamsortWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
+    # node: [
+    #   "node_modules/webpack/bin/webpack.js",
+    #   "--mode",
+    #   "development",
+    #   "--watch-stdin",
+    #   cd: Path.expand("../assets", __DIR__)
+    # ],
+    # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
 
 # ## SSL Support
