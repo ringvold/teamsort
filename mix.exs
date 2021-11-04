c8f4446068e4981e42c655f5873a0f7a1ea3f889
Harald Ringvold <harald.ringvold@gmail.com>
2021-11-04 02:29:19 +0100

    Release version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Teamsort.MixProject do
   def project do
     [
       app: :teamsort,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
